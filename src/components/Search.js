f149a10e09e06f5a736305352443015877c24c44
Rachel Kiarie <rachelnnk94@gmail.com>
2022-07-25 17:09:11 +0300

    Add search functionality, order appointments according to pet name in ascending order.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,10 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+import { useState } from 'react'
 
-const DropDown = () => {
+const DropDown = ({toggle}) => {
+  if(!toggle) {
+    return null;
+  }
   return (
     <div className="origin-top-right absolute right-0 mt-2 w-56
     rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -25,7 +29,9 @@ const DropDown = () => {
   )
 
 }
-const Search = () => {
+const Search = ({query, onQueryChange}) => {
+  let [toggleSort, setToggleSort] = useState(false)
+  
   return(
 
     <div className="py-5">
@@ -34,15 +40,17 @@ const Search = () => {
         <BiSearch />
         <label htmlFor="query" className="sr-only" />
       </div>
-      <input type="text" name="query" id="query" value=""
+      <input type="text" name="query" id="query" value={query} 
+      // eslint-disable-next-line no-undef
+      onChange={(event) => {onQueryChange(event.target.value)}}
         className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
       <div className="absolute inset-y-0 right-0 flex items-center">
         <div>
-          <button type="button"
+          <button type="button" onClick={() =>{ setToggleSort(!toggleSort)} }
             className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
             Sort By <BiCaretDown className="ml-2" />
           </button>
-          <DropDown />
+          <DropDown toggle={toggleSort} />
         </div>
       </div>
     </div>
